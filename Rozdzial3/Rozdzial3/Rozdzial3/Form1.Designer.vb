e1a7c87383ae6bebe31c0d7f7ca8c4e09a11bd45
pmkulakowski <pm.kulakowski@gmail.com>
2020-06-07 14:06:03 +0200

    VB.Net Cwiczenia - Rozdzial 3

---
+++ Rozdzial3/Rozdzial3/Rozdzial3/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmZmienne
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPierwszaLiczba = New System.Windows.Forms.TextBox()
+        Me.txtDrugaLiczba = New System.Windows.Forms.TextBox()
+        Me.lblWynik = New System.Windows.Forms.Label()
+        Me.cmdPomnoz = New System.Windows.Forms.Button()
+        Me.cmdVat = New System.Windows.Forms.Button()
+        Me.cmdGodziny = New System.Windows.Forms.Button()
+        Me.cmdRozne = New System.Windows.Forms.Button()
+        Me.cmdWieksza = New System.Windows.Forms.Button()
+        Me.cmdModulo = New System.Windows.Forms.Button()
+        Me.cmdAnd = New System.Windows.Forms.Button()
+        Me.cmdOrElse = New System.Windows.Forms.Button()
+        Me.txtImie = New System.Windows.Forms.TextBox()
+        Me.cmdSS = New System.Windows.Forms.Button()
+        Me.cmdSI = New System.Windows.Forms.Button()
+        Me.cmdLicznik = New System.Windows.Forms.Button()
+        Me.cmdStruktura = New System.Windows.Forms.Button()
+        Me.CmdTablica1 = New System.Windows.Forms.Button()
+        Me.cmdTablica2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtPierwszaLiczba
+        '
+        Me.txtPierwszaLiczba.Location = New System.Drawing.Point(8, 16)
+        Me.txtPierwszaLiczba.Name = "txtPierwszaLiczba"
+        Me.txtPierwszaLiczba.Size = New System.Drawing.Size(112, 20)
+        Me.txtPierwszaLiczba.TabIndex = 0
+        Me.txtPierwszaLiczba.Text = "0"
+        '
+        'txtDrugaLiczba
+        '
+        Me.txtDrugaLiczba.Location = New System.Drawing.Point(8, 48)
+        Me.txtDrugaLiczba.Name = "txtDrugaLiczba"
+        Me.txtDrugaLiczba.Size = New System.Drawing.Size(112, 20)
+        Me.txtDrugaLiczba.TabIndex = 1
+        Me.txtDrugaLiczba.Text = "0"
+        '
+        'lblWynik
+        '
+        Me.lblWynik.AutoSize = True
+        Me.lblWynik.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblWynik.Location = New System.Drawing.Point(8, 112)
+        Me.lblWynik.MinimumSize = New System.Drawing.Size(112, 20)
+        Me.lblWynik.Name = "lblWynik"
+        Me.lblWynik.Size = New System.Drawing.Size(112, 20)
+        Me.lblWynik.TabIndex = 2
+        '
+        'cmdPomnoz
+        '
+        Me.cmdPomnoz.Location = New System.Drawing.Point(176, 16)
+        Me.cmdPomnoz.Name = "cmdPomnoz"
+        Me.cmdPomnoz.Size = New System.Drawing.Size(112, 22)
+        Me.cmdPomnoz.TabIndex = 3
+        Me.cmdPomnoz.Text = "Mnożenie"
+        Me.cmdPomnoz.UseVisualStyleBackColor = True
+        '
+        'cmdVat
+        '
+        Me.cmdVat.Location = New System.Drawing.Point(128, 16)
+        Me.cmdVat.Name = "cmdVat"
+        Me.cmdVat.Size = New System.Drawing.Size(40, 22)
+        Me.cmdVat.TabIndex = 4
+        Me.cmdVat.Text = "VAT"
+        Me.cmdVat.UseVisualStyleBackColor = True
+        '
+        'cmdGodziny
+        '
+        Me.cmdGodziny.Location = New System.Drawing.Point(128, 48)
+        Me.cmdGodziny.Name = "cmdGodziny"
+        Me.cmdGodziny.Size = New System.Drawing.Size(40, 22)
+        Me.cmdGodziny.TabIndex = 5
+        Me.cmdGodziny.Text = "Godz."
+        Me.cmdGodziny.UseVisualStyleBackColor = True
+        '
+        'cmdRozne
+        '
+        Me.cmdRozne.Location = New System.Drawing.Point(16, 168)
+        Me.cmdRozne.Name = "cmdRozne"
+        Me.cmdRozne.Size = New System.Drawing.Size(32, 24)
+        Me.cmdRozne.TabIndex = 6
+        Me.cmdRozne.Text = "<>"
+        Me.cmdRozne.UseVisualStyleBackColor = True
+        '
+        'cmdWieksza
+        '
+        Me.cmdWieksza.Location = New System.Drawing.Point(72, 168)
+        Me.cmdWieksza.Name = "cmdWieksza"
+        Me.cmdWieksza.Size = New System.Drawing.Size(32, 24)
+        Me.cmdWieksza.TabIndex = 7
+        Me.cmdWieksza.Text = ">"
+        Me.cmdWieksza.UseVisualStyleBackColor = True
+        '
+        'cmdModulo
+        '
+        Me.cmdModulo.Location = New System.Drawing.Point(128, 168)
+        Me.cmdModulo.Name = "cmdModulo"
+        Me.cmdModulo.Size = New System.Drawing.Size(32, 24)
+        Me.cmdModulo.TabIndex = 8
+        Me.cmdModulo.Text = "%"
+        Me.cmdModulo.UseVisualStyleBackColor = True
+        '
+        'cmdAnd
+        '
+        Me.cmdAnd.Location = New System.Drawing.Point(184, 168)
+        Me.cmdAnd.Name = "cmdAnd"
+        Me.cmdAnd.Size = New System.Drawing.Size(48, 24)
+        Me.cmdAnd.TabIndex = 9
+        Me.cmdAnd.Text = "And"
+        Me.cmdAnd.UseVisualStyleBackColor = True
+        '
+        'cmdOrElse
+        '
+        Me.cmdOrElse.Location = New System.Drawing.Point(252, 168)
+        Me.cmdOrElse.Name = "cmdOrElse"
+        Me.cmdOrElse.Size = New System.Drawing.Size(48, 24)
+        Me.cmdOrElse.TabIndex = 10
+        Me.cmdOrElse.Text = "OrElse"
+        Me.cmdOrElse.UseVisualStyleBackColor = True
+        '
+        'txtImie
+        '
+        Me.txtImie.Location = New System.Drawing.Point(8, 80)
+        Me.txtImie.Name = "txtImie"
+        Me.txtImie.Size = New System.Drawing.Size(112, 20)
+        Me.txtImie.TabIndex = 11
+        Me.txtImie.Text = "Eleonora"
+        '
+        'cmdSS
+        '
+        Me.cmdSS.Location = New System.Drawing.Point(184, 48)
+        Me.cmdSS.Name = "cmdSS"
+        Me.cmdSS.Size = New System.Drawing.Size(40, 22)
+        Me.cmdSS.TabIndex = 12
+        Me.cmdSS.Text = "S+S"
+        Me.cmdSS.UseVisualStyleBackColor = True
+        '
+        'cmdSI
+        '
+        Me.cmdSI.Location = New System.Drawing.Point(240, 48)
+        Me.cmdSI.Name = "cmdSI"
+        Me.cmdSI.Size = New System.Drawing.Size(40, 22)
+        Me.cmdSI.TabIndex = 13
+        Me.cmdSI.Text = "S+I"
+        Me.cmdSI.UseVisualStyleBackColor = True
+        '
+        'cmdLicznik
+        '
+        Me.cmdLicznik.Location = New System.Drawing.Point(176, 80)
+        Me.cmdLicznik.Name = "cmdLicznik"
+        Me.cmdLicznik.Size = New System.Drawing.Size(112, 22)
+        Me.cmdLicznik.TabIndex = 14
+        Me.cmdLicznik.Text = "Kliknij"
+        Me.cmdLicznik.UseVisualStyleBackColor = True
+        '
+        'cmdStruktura
+        '
+        Me.cmdStruktura.Location = New System.Drawing.Point(128, 80)
+        Me.cmdStruktura.Name = "cmdStruktura"
+        Me.cmdStruktura.Size = New System.Drawing.Size(40, 22)
+        Me.cmdStruktura.TabIndex = 15
+        Me.cmdStruktura.Text = "Typ"
+        Me.cmdStruktura.UseVisualStyleBackColor = True
+        '
+        'CmdTablica1
+        '
+        Me.CmdTablica1.Location = New System.Drawing.Point(128, 112)
+        Me.CmdTablica1.Name = "CmdTablica1"
+        Me.CmdTablica1.Size = New System.Drawing.Size(72, 22)
+        Me.CmdTablica1.TabIndex = 16
+        Me.CmdTablica1.Text = "Tablica 1W"
+        Me.CmdTablica1.UseVisualStyleBackColor = True
+        '
+        'cmdTablica2
+        '
+        Me.cmdTablica2.Location = New System.Drawing.Point(216, 112)
+        Me.cmdTablica2.Name = "cmdTablica2"
+        Me.cmdTablica2.Size = New System.Drawing.Size(72, 22)
+        Me.cmdTablica2.TabIndex = 17
+        Me.cmdTablica2.Text = "Tablica 2W"
+        Me.cmdTablica2.UseVisualStyleBackColor = True
+        '
+        'frmZmienne
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(386, 212)
+        Me.Controls.Add(Me.cmdTablica2)
+        Me.Controls.Add(Me.CmdTablica1)
+        Me.Controls.Add(Me.cmdStruktura)
+        Me.Controls.Add(Me.cmdLicznik)
+        Me.Controls.Add(Me.cmdSI)
+        Me.Controls.Add(Me.cmdSS)
+        Me.Controls.Add(Me.txtImie)
+        Me.Controls.Add(Me.cmdOrElse)
+        Me.Controls.Add(Me.cmdAnd)
+        Me.Controls.Add(Me.cmdModulo)
+        Me.Controls.Add(Me.cmdWieksza)
+        Me.Controls.Add(Me.cmdRozne)
+        Me.Controls.Add(Me.cmdGodziny)
+        Me.Controls.Add(Me.cmdVat)
+        Me.Controls.Add(Me.cmdPomnoz)
+        Me.Controls.Add(Me.lblWynik)
+        Me.Controls.Add(Me.txtDrugaLiczba)
+        Me.Controls.Add(Me.txtPierwszaLiczba)
+        Me.Location = New System.Drawing.Point(16, 168)
+        Me.Name = "frmZmienne"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Poznajemy zmienne"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtPierwszaLiczba As System.Windows.Forms.TextBox
+    Friend WithEvents txtDrugaLiczba As System.Windows.Forms.TextBox
+    Friend WithEvents lblWynik As System.Windows.Forms.Label
+    Friend WithEvents cmdPomnoz As System.Windows.Forms.Button
+    Friend WithEvents cmdVat As System.Windows.Forms.Button
+    Friend WithEvents cmdGodziny As System.Windows.Forms.Button
+    Friend WithEvents cmdRozne As System.Windows.Forms.Button
+    Friend WithEvents cmdWieksza As System.Windows.Forms.Button
+    Friend WithEvents cmdModulo As System.Windows.Forms.Button
+    Friend WithEvents cmdAnd As System.Windows.Forms.Button
+    Friend WithEvents cmdOrElse As System.Windows.Forms.Button
+    Friend WithEvents txtImie As System.Windows.Forms.TextBox
+    Friend WithEvents cmdSS As System.Windows.Forms.Button
+    Friend WithEvents cmdSI As System.Windows.Forms.Button
+    Friend WithEvents cmdLicznik As System.Windows.Forms.Button
+    Friend WithEvents cmdStruktura As System.Windows.Forms.Button
+    Friend WithEvents CmdTablica1 As System.Windows.Forms.Button
+    Friend WithEvents cmdTablica2 As System.Windows.Forms.Button
+
+End Class
---
+++ Rozdzial3/Rozdzial3/Rozdzial3/Form1.vb
@@ -0,0 +1,150 @@
+﻿Public Class frmZmienne
+
+    Dim intArgument1, intArgument2 As Integer
+    Dim strAgrument1, strArgument2 As String
+    Dim decWynik As Decimal = 0.0000001
+    Const VAT As Single = 0.22
+    Const GODZINY As Short = 60
+    Dim blnTest As Boolean
+    Dim strImie As String
+
+    Private Structure Osoba
+        Dim Imie As String
+        Dim DataUr As Date
+        Dim Pensja As Double
+    End Structure
+
+    Private Sub txtPierwszaLiczba_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtPierwszaLiczba.KeyPress, txtDrugaLiczba.KeyPress
+        If e.KeyChar < "0" Or e.KeyChar > "9" Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtPierwszaLiczba_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtPierwszaLiczba.TextChanged
+        intArgument1 = CInt(txtPierwszaLiczba.Text)
+    End Sub
+
+
+    Private Sub txtDrugaLiczba_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtDrugaLiczba.TextChanged
+        intArgument2 = CInt(txtDrugaLiczba.Text)
+    End Sub
+
+    Private Sub cmdPomnoz_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdPomnoz.Click
+        decWynik = intArgument1 * intArgument2
+        lblWynik.Text = decWynik
+    End Sub
+
+    Private Sub frmZmienne_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        lblWynik.Text = decWynik
+    End Sub
+
+    Private Sub cmdVat_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdVat.Click
+        decWynik = intArgument1 * VAT
+        lblWynik.Text = decWynik
+    End Sub
+
+    Private Sub cmdGodziny_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdGodziny.Click
+        decWynik = intArgument2 / GODZINY
+        lblWynik.Text = decWynik
+    End Sub
+
+    Private Sub cmdRozne_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdRozne.Click
+        blnTest = intArgument1 <> intArgument2
+        lblWynik.Text = blnTest
+    End Sub
+
+    Private Sub cmdWieksza_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdWieksza.Click
+        blnTest = intArgument1 > intArgument2
+        lblWynik.Text = blnTest
+    End Sub
+
+    Private Sub cmdModulo_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdModulo.Click
+        decWynik = intArgument1 Mod intArgument2
+        lblWynik.Text = decWynik
+    End Sub
+
+    Private Sub cmdAnd_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdAnd.Click
+        blnTest = intArgument1 Mod 2 = 0 And intArgument2 Mod 2 = 0
+        lblWynik.Text = blnTest
+    End Sub
+
+    Private Sub cmdOrElse_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdOrElse.Click
+        blnTest = intArgument1 > intArgument2 OrElse intArgument1 * 2 > intArgument2 * 3
+        lblWynik.Text = blnTest
+    End Sub
+
+    Private Sub txtImie_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtImie.TextChanged
+        strImie = txtImie.Text
+        lblWynik.Text = strImie
+    End Sub
+
+    Private Sub cmdSS_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdSS.Click
+        strAgrument1 = txtPierwszaLiczba.Text
+        strArgument2 = txtDrugaLiczba.Text
+        lblWynik.Text = strAgrument1 + strArgument2
+    End Sub
+
+    Private Sub cmdSI_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdSI.Click
+        strAgrument1 = txtPierwszaLiczba.Text
+        lblWynik.Text = strAgrument1 + intArgument2
+    End Sub
+
+    Private Sub cmdLicznik_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdLicznik.Click
+        Static intLicznik As Integer
+        intLicznik += 1
+
+        If intLicznik > 0 Then
+            Dim intParzyste As Integer
+            intParzyste = intLicznik Mod 2
+            lblWynik.Text = intParzyste
+        End If
+        lblWynik.Text = intLicznik
+    End Sub
+
+    Private Sub cmdStruktura_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdStruktura.Click
+        Dim Szef As Osoba
+
+        With Szef
+            .Imie = txtImie.Text
+            .DataUr = #2/12/1979#
+            .Pensja = 6666.98
+        End With
+
+        Dim NajlepszyPracownik As Osoba
+
+        With NajlepszyPracownik
+            .Imie = txtImie.Text + "a"
+            .DataUr = #5/25/1985#
+            .Pensja = 666.08
+        End With
+
+        lblWynik.Text = Szef.Imie + " " + Szef.Pensja.ToString
+
+        MsgBox(NajlepszyPracownik.Imie + " " + NajlepszyPracownik.Pensja.ToString)
+    End Sub
+
+    Private Sub CmdTablica1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles CmdTablica1.Click
+        Dim Osoby(4) As String
+        Osoby(0) = "Ala"
+        Osoby(1) = "Basia"
+        Osoby(2) = "Kasia"
+        Osoby(3) = "Marysia"
+        Osoby(4) = "Zosia"
+        lblWynik.Text = Osoby(3)
+
+    End Sub
+
+    Private Sub cmdTablica2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdTablica2.Click
+        Dim Liczby(4, 9) As Integer
+        Liczby(0, 0) = 2
+        Liczby(0, 7) = 4
+        Liczby(1, 2) = 8
+        Liczby(1, 9) = 16
+        Liczby(4, 9) = 32
+        lblWynik.Text = Liczby(1, 2)
+
+        ReDim Preserve Liczby(4, 14)
+        Liczby(4, 14) = 64
+        lblWynik.Text = Liczby(4, 14)
+    End Sub
+End Class
