776f20882ddcacd1ed294567e311210fe3a9c546
Przemysław Kułakiwski <pm.kulakowski@gmail.com>
2020-06-24 20:00:25 +0200

    VB.Net Cwiczenia - Rozdzial 9

---
+++ Rozdzial9/Rozdzial9/Rozdzial9/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBledy
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdSilnia = New System.Windows.Forms.Button()
+        Me.cmdOtworz = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmdSilnia
+        '
+        Me.cmdSilnia.Location = New System.Drawing.Point(16, 16)
+        Me.cmdSilnia.Name = "cmdSilnia"
+        Me.cmdSilnia.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSilnia.TabIndex = 0
+        Me.cmdSilnia.Text = "Silnia"
+        Me.cmdSilnia.UseVisualStyleBackColor = True
+        '
+        'cmdOtworz
+        '
+        Me.cmdOtworz.Location = New System.Drawing.Point(154, 16)
+        Me.cmdOtworz.Name = "cmdOtworz"
+        Me.cmdOtworz.Size = New System.Drawing.Size(75, 23)
+        Me.cmdOtworz.TabIndex = 1
+        Me.cmdOtworz.Text = "Otwórz"
+        Me.cmdOtworz.UseVisualStyleBackColor = True
+        '
+        'frmBledy
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(312, 73)
+        Me.Controls.Add(Me.cmdOtworz)
+        Me.Controls.Add(Me.cmdSilnia)
+        Me.Name = "frmBledy"
+        Me.Text = "Wyszukujemy i przechwytujemy błędy"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents cmdSilnia As System.Windows.Forms.Button
+    Friend WithEvents cmdOtworz As System.Windows.Forms.Button
+
+End Class
---
+++ Rozdzial9/Rozdzial9/Rozdzial9/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class frmBledy
+
+
+    Private Sub cmdSilnia_Click(sender As Object, e As System.EventArgs) Handles cmdSilnia.Click
+        Dim intLiczba As Integer
+
+        intLiczba = InputBox("Podaj liczbę, której silnia ma zostać wyliczona", , 1)
+
+        Try
+            MsgBox("Wynik: " + SilniaRekurencja(intLiczba).ToString)
+        Catch ex As Exception
+            MsgBox("Wystąpił błąd: " + ex.Message + vbCrLf + "Wywołany przez: " + ex.StackTrace)
+        Finally
+            Static licznikWywolan As Integer
+            licznikWywolan += 1
+            MsgBox("Licznik wywolan: " + licznikWywolan.ToString)
+        End Try
+
+        'Debug.Assert(intLiczba > 1)
+        MsgBox("Wynik: " + SilniaIteracja(intLiczba).ToString)
+
+
+    End Sub
+
+    Function SilniaIteracja(ByVal liczba As Integer) As Double
+
+        Dim decWynik As Decimal = 1
+        Dim licznik As Integer
+
+        For licznik = 1 To liczba
+            decWynik *= licznik
+        Next
+        Return decWynik
+    End Function
+
+    Function SilniaRekurencja(ByVal liczba As Integer) As Double
+
+        Dim dblWynik As Double = 1
+        If liczba < 0 Then
+            Throw New ArgumentOutOfRangeException("Ujemny argument")
+        Else
+            dblWynik = liczba * SilniaRekurencja(liczba - 1)
+        End If
+        Return dblWynik
+
+    End Function
+
+    Private Sub cmdOtworz_Click(sender As System.Object, e As System.EventArgs) Handles cmdOtworz.Click
+        Try
+            Dim fs As New System.IO.FileStream("G:\plik.txt", IO.FileMode.Open)
+        Catch ex As System.IO.FileNotFoundException
+            MsgBox("Nie znaleziono określonego pliku")
+        Catch ex As System.IO.DirectoryNotFoundException
+            MsgBox("Nieprawidłowa lokalizacja pliku")
+        Catch ex As System.UnauthorizedAccessException
+            MsgBox("Odmowa dostępu")
+        Catch ex As System.IO.FileLoadException
+            MsgBox("Uszkodzony plik z danymi")
+        Catch ex As Exception
+            MsgBox("Nieoczekiwany błąd " + ex.Message)
+        End Try
+    End Sub
+End Class
